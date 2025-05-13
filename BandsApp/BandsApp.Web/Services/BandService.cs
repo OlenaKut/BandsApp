b1df4005af377738b16918a1014b8fdf00664d8e
Iryna <iryna.chorna@hotmail.com>
2025-05-13 12:02:07 +0200

    Added Layout and ViewStart

--- BandsApp/BandsApp.Web/Services/BandService.cs
+++ BandsApp/BandsApp.Web/Services/BandService.cs
@@ -5,7 +5,7 @@ namespace BandsApp.Web.Services;
 public class BandService
 {
     List<Band> bands = [
-        new Band { Id = 1, Name = "Beatles", Description = "Pop and rock bands", ImageName = "theBeatles.jpeg" },
+        new Band { Id = 1, Name = "Beatles", Description = "Pop and rock bands", ImageName = "theBeatles.jpg" },
         new Band { Id = 2, Name = "Depeche Mode", Description = "English electronic band", ImageName = "depecheMode.jpg" },
         new Band { Id = 3, Name = "ABBA", Description = "Swedish pop group", ImageName = "abba.jpg" },
         ];
