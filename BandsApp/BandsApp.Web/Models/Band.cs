edb180ec354315af02148222583cfb2e2598df44
bror <reachingbror@gmail.com>
2025-05-13 11:28:32 +0200

    Added images

--- BandsApp/BandsApp.Web/Models/Band.cs
+++ BandsApp/BandsApp.Web/Models/Band.cs
@@ -5,5 +5,6 @@ public class Band
     public int Id { get; set; }
     public string Name { get; set; } = string.Empty;
     public string Description { get; set; } = string.Empty;
+    public string ImageName { get; set; } = string.Empty;
 }
 
--- BandsApp/BandsApp.Web/Services/BandService.cs
+++ BandsApp/BandsApp.Web/Services/BandService.cs
@@ -5,9 +5,9 @@ namespace BandsApp.Web.Services;
 public class BandService
 {
     List<Band> bands = [
-        new Band { Id = 1, Name = "Beatles", Description = "Pop and rock bands" },
-        new Band { Id = 2, Name = "Depeche Mode", Description = "English electronic band" },
-        new Band { Id = 3, Name = "ABBA", Description = "Swedish pop group" },
+        new Band { Id = 1, Name = "Beatles", Description = "Pop and rock bands", ImageName = "theBeatles.jpeg" },
+        new Band { Id = 2, Name = "Depeche Mode", Description = "English electronic band", ImageName = "depecheMode.jpg" },
+        new Band { Id = 3, Name = "ABBA", Description = "Swedish pop group", ImageName = "abba.jpg" },
         ];
 
     public Band? GetBandById(int id)
