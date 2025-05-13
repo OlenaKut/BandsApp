ff7a09486dd3cf29a198b9a757c4bd3d62ca084f
Iryna <iryna.chorna@hotmail.com>
2025-05-13 10:11:48 +0200

    Added Controllers

---
+++ BandsApp/BandsApp.Web/Controllers/BandsController.cs
@@ -0,0 +1,16 @@
+﻿using BandsApp.Web.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BandsApp.Web.Controllers;
+
+public class BandsController : Controller
+{
+    BandService bandService = new();
+
+    [Route("/")]
+    public IActionResult Index()
+    {
+        var bands = bandService.GetAllBands();
+        return View(bands);
+    }
+}
---
+++ BandsApp/BandsApp.Web/Services/BandService.cs
@@ -0,0 +1,21 @@
+﻿using BandsApp.Web.Models;
+
+namespace BandsApp.Web.Services;
+
+public class BandService
+{
+    List<Band> bands = [
+        new Band { Id = 1, Name = "Beatles", Description = "Pop and rock bands" },
+        new Band { Id = 2, Name = "Depeche Mode", Description = "English electronic band" },
+        new Band { Id = 3, Name = "ABBA", Description = "Swedish pop group" },
+        ];
+
+    public Band? GetBandById(int id)
+    {
+        return bands.SingleOrDefault(b => b.Id == id);
+    }
+
+    public Band[] GetAllBands() => bands
+        .OrderBy(b => b.Name)
+        .ToArray();
+}
